d05017cc9743e635b2ec49868067c6e4b9816887
Uku.Tammet <uku.tammet@ttu.ee>
2019-05-20 23:47:20 +0300

    Updates

---
+++ precache-manifest.b850f2fee0fd356be916bb99aa97357d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6d71985fc4ae8ba82132f79b9257bea",
+    "url": "/simulus/index.html"
+  },
+  {
+    "revision": "cd2463984f6f06303bf4",
+    "url": "/simulus/static/css/2.8cef481d.chunk.css"
+  },
+  {
+    "revision": "cd2463984f6f06303bf4",
+    "url": "/simulus/static/js/2.77fb0a62.chunk.js"
+  },
+  {
+    "revision": "cb5282db6d1dab5f01ac",
+    "url": "/simulus/static/js/main.adab9681.chunk.js"
+  },
+  {
+    "revision": "ce2ac3696a035f1693db",
+    "url": "/simulus/static/js/runtime~main.088bac39.js"
+  },
+  {
+    "url": "/simulus/static/media/0ab54153eeeca0ce03978cc463b257f7.0ab54153.woff2"
+  },
+  {
+    "url": "/simulus/static/media/13db00b7a34fee4d819ab7f9838cc428.13db00b7.eot"
+  },
+  {
+    "url": "/simulus/static/media/701ae6abd4719e9c2ada3535a497b341.701ae6ab.eot"
+  },
+  {
+    "url": "/simulus/static/media/82f60bd0b94a1ed68b1e6e309ce2e8c3.82f60bd0.svg"
+  },
+  {
+    "url": "/simulus/static/media/8e3c7f5520f5ae906c6cf6d7f3ddcd19.8e3c7f55.eot"
+  },
+  {
+    "url": "/simulus/static/media/962a1bf31c081691065fe333d9fa8105.962a1bf3.svg"
+  },
+  {
+    "url": "/simulus/static/media/9c74e172f87984c48ddf5c8108cabe67.9c74e172.png"
+  },
+  {
+    "url": "/simulus/static/media/a046592bac8f2fd96e994733faf3858c.a046592b.woff"
+  },
+  {
+    "url": "/simulus/static/media/a1a749e89f578a49306ec2b055c073da.a1a749e8.svg"
+  },
+  {
+    "url": "/simulus/static/media/ad97afd3337e8cda302d10ff5a4026b8.ad97afd3.ttf"
+  },
+  {
+    "url": "/simulus/static/media/b87b9ba532ace76ae9f6edfe9f72ded2.b87b9ba5.ttf"
+  },
+  {
+    "url": "/simulus/static/media/c5ebe0b32dc1b5cc449a76c4204d13bb.c5ebe0b3.ttf"
+  },
+  {
+    "url": "/simulus/static/media/cd6c777f1945164224dee082abaea03a.cd6c777f.woff2"
+  },
+  {
+    "url": "/simulus/static/media/e8c322de9658cbeb8a774b6624167c2c.e8c322de.woff2"
+  },
+  {
+    "url": "/simulus/static/media/ef60a4f6c25ef7f39f2d25a748dbecfe.ef60a4f6.woff"
+  },
+  {
+    "url": "/simulus/static/media/faff92145777a3cbaf8e7367b4807987.faff9214.woff"
+  }
+]);
